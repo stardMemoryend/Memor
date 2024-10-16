410de87ebfee777211c41e51dcd943b391ebbd46
Memor <161856323+stardMemoryend@users.noreply.github.com>
2024-10-16 20:35:14 +0800

    KNN.py

--- Assignment 1/KNN.py
+++ Assignment 1/KNN.py
@@ -74,11 +74,55 @@ def knn(X_train, y_train, X_test, k):
         # 预测标签
         predictions.append(most_common_label)
     return torch.cat(predictions)
+# k折交叉验证
+def cross_validate(X, y, k_folds, k_neighbors):
+    fold_size = len(X) // k_folds  # 每个子集包含的样本数
+    accuracies = []
+
+    for fold in range(k_folds):
+        start, end = fold * fold_size, (fold + 1) * fold_size
+        X_val, y_val = X[start:end], y[start:end]   # 测试集
+        X_train = torch.cat((X[:start], X[end:]))   # 训练集
+        y_train = torch.cat((y[:start], y[end:]))
+
+        # 预测验证集标签
+        predicted_labels = knn(X_train, y_train, X_val, k_neighbors)
+
+        # 准确率
+        accuracy = (predicted_labels == y_val).float().mean()
+        accuracies.append(accuracy)
+
+    mean_accuracy = torch.tensor(accuracies).mean()
+    return mean_accuracy
+
 # 预测测试集标签
-predicted_labels = knn(train_images, train_labels, test_images, k=4)
+# predicted_labels = knn(train_images, train_labels, test_images, k=4)
+
 
 # 计算准确率
 # np.mean用于计算数组的平均值，predicted_labels == test_labels会得到布尔值0和1
 # True为1，False为0
-accuracy = (predicted_labels == test_labels).float().mean()
-print(accuracy)
+# accuracy = (predicted_labels == test_labels).float().mean()
+# print(accuracy)
+
+# 定义可能的超参数
+k_values = [1, 2, 3, 4, 5]
+
+# 网格搜索
+best_k = None
+best_accuracy = 0.0
+
+for k in k_values:
+    accuracy = cross_validate(train_images, train_labels, 5, k)
+    print(f'K={k}, accuracy: {accuracy}')
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        best_k = k
+
+print('\n')
+print(best_k, best_accuracy)
+
+pred = knn(train_images, train_labels, test_images, best_k)
+
+true_accuracy = (pred == test_labels).float().mean()
+print(true_accuracy)
